8e5dd08109a286825fe0701df9ff8916cd6b1519
ioseph01 <j0ni0n.clang@gmail.com>
2026-04-10 14:08:50 -0700

    sorting model and testing

---
+++ api.py
@@ -0,0 +1,237 @@
+from utils.hash import hash_key
+from Structures.file_objcts import Page, MetaData
+from config import M, PAGE_SIZE
+import bisect
+import uuid
+
+class DFS:
+  def __init__(self, chord, entry_node):
+    self.chord = chord
+    self.entry_node = entry_node
+
+  def metadata_key(self, file_name : str):
+    return hash_key("metadata:" + file_name)
+
+  def page_key(self, file_name : str, page_number):
+    return hash_key(file_name + ":" + str(page_number))
+  
+  def get_metadata(self, file_name):
+    key = self.metadata_key(file_name)
+    obj = self.entry_node.get(key)
+
+    if obj is None:
+      return None
+    elif isinstance(obj, dict):
+      return MetaData._export(obj)
+    return obj
+  
+  def set_metadata(self, metadata):
+    self.entry_node.set(metadata.key, metadata)
+
+  def get_page(self, page_key : int):
+    obj = self.entry_node.get(page_key)
+
+    if obj is None:
+      return None
+    elif isinstance(obj, dict):
+      return Page._export(obj)
+    return obj
+  
+
+  def append_contents(self, file_name: str, contents: str):
+    ''' Append string to file '''
+    metadata = self.get_metadata(file_name)
+    if metadata is None:
+      return f"ERROR: File '{file_name}' not found"
+    chunks = [contents[i:i + PAGE_SIZE] for i in range(0, len(contents), PAGE_SIZE)]
+    if not chunks:
+      return f"ERROR: Nothing to append"
+    
+    for chunk in chunks:
+        page_number = metadata.page_count
+        page_key = self.page_key(file_name, page_number)
+        page = Page(page_key, chunk, page_number)
+        self.entry_node.set(page_key, page)
+        metadata.page_keys.append(page_key)
+        metadata.file_size += len(chunk)
+
+    self.set_metadata(metadata)
+    return f"SUCCESS: appended {len(chunks)} page(s) to '{file_name}'"
+
+
+  """ Required DFS Operations """
+
+  def touch(self, file_name : str):
+    ''' Create empty file '''
+    existing = self.get_metadata(file_name)
+    if existing is not None:
+      return f"ERROR: File '{file_name}' already exists"
+    
+    key = self.metadata_key(file_name)
+    metadata = MetaData(key, file_name, [], 0)
+    self.set_metadata(metadata)
+    return f"SUCCESS: '{file_name}' created"
+  
+
+  def append(self, file_name : str, local_path : str):
+    ''' Read a local file and append contents '''
+    try:
+      with open(local_path, "r") as f:
+        contents = f.read()
+    except FileNotFoundError:
+      return f"ERROR: File '{local_path}' not found"
+  
+    metadata = self.get_metadata(file_name)
+    if metadata is None:
+      self.touch(file_name)
+      metadata = self.get_metadata(file_name)
+
+    chunks = [contents[i:i + PAGE_SIZE] for i in range(0, len(contents), PAGE_SIZE)]
+    if not chunks:
+      return f"FAILURE: File '{local_path}' is empty, nothing appended"
+    
+    for chunk in chunks:
+      page_number = metadata.page_count
+      page_key = self.page_key(file_name, page_number)
+      page = Page(page_key, chunk, page_number)
+
+      self.entry_node.set(page_key, page)
+      metadata.page_keys.append(page_key)
+      metadata.file_size += 1
+
+    self.set_metadata(metadata)
+    return f"SUCCESS: Appended {len(chunks)} to '{file_name}'"
+  
+
+  def read(self, file_name: str):
+    ''' Read and return file contents '''
+    metadata = self.get_metadata(file_name)
+    if metadata is None:
+      return f"ERROR: File '{file_name}' not found"
+    elif metadata.page_count == 0:
+      return ""
+    
+    contents = []
+    for key in metadata.page_keys:
+      page = self.get_page(key)
+      if page is None:
+        return f"ERROR: page {key} missing for '{file_name}'"
+      contents.append(page.contents)
+
+    return "".join(contents)
+  
+
+  def head(self, file_name: str, n: int):
+    ''' Return first n lines of a file '''
+    contents = self.read(file_name)
+    if contents[:5] == "ERROR":
+      return contents
+    
+    lines = contents.splitlines()
+    return "\n".join(lines[:n])
+  
+
+  def tail(self, file_name: str, n: int):
+    ''' Return last n lines of a file '''
+    contents = self.read(file_name)
+    if contents[:5] == "ERROR":
+      return contents
+    
+    lines = contents.splitlines()
+    return "\n".join(lines[-n:])
+  
+
+  def delete_file(self, file_name: str):
+    ''' Delete all pages and metadata of a file '''
+    metadata = self.get_metadata(file_name)
+
+    if metadata is None:
+      return f"ERROR: File '{file_name}' not found"
+    
+    for page_key in metadata.page_keys:
+      self.entry_node.delete(page_key)
+
+    self.entry_node.delete(metadata.key)
+    return f"SUCCESS: '{file_name}' deleted"
+  
+
+  def ls(self):
+    ''' List all files in chord '''
+    files = []
+    for node in self.chord.nodes.values():
+      for key, obj in node.store.items():
+        if obj.type == 'MetaData':
+          files.append(obj.file_name)
+
+    if not files:
+      return "Directory empty"
+    return "\n".join(sorted(files))
+    
+
+  def stat(self, file_name : str):
+    ''' Return metadata for a file '''
+    metadata = self.get_metadata(file_name)
+    if metadata is None:
+      return f"ERROR: File '{file_name}' does not exist"
+    
+    info = metadata._export()
+    lines = [f"{k}: {v}" for k, v in info.items()]
+    return "\n".join(lines)
+  
+
+  def sort_file(self, file_name : str, output_filename : str):
+    metadata = self.get_metadata(file_name)
+    if metadata is None:
+      return f"ERROR: File '{file_name}' does not exist"
+    elif metadata.page_count == 0:
+        return f"ERROR: File '{file_name}' is empty"
+    
+    # Get pages and parse
+    records = []
+    for page_key in metadata.page_keys:
+      page = self.get_page(page_key)
+      if page is None:
+        return f"ERROR: Page missing in file '{file_name}'"
+      for line in page.contents.splitlines():
+        line = line.strip()
+        if line:
+          if "," not in line:
+            return f"ERROR: invalid record format '{line}', expected key,value"
+          k,v = line.split(",", 1)
+          records.append((k.strip(), v.strip()))
+
+    if not records:
+      return f"ERROR: no valid records found in '{file_name}'"
+    
+    # Sort
+    job_id = str(uuid.uuid4())
+    for k,v in records:
+      target_key = hash_key(k)
+      target_node = self.chord.find_succ(target_key)
+      if job_id not in target_node.sort_buffer:
+        target_node.sort_buffer[job_id] = []
+      bisect.insort(target_node.sort_buffer[job_id], (k,v))
+
+    # Store
+    sorted_records = []
+    for id in self.chord.sorted_ids:
+      node = self.chord.nodes[id]
+      if job_id in node.sort_buffer:
+        sorted_records.extend(node.sort_buffer[job_id])
+    sorted_records.sort(key=lambda pair: pair[0])
+
+    # Output
+    contents = "\n".join(f"{k},{v}" for k,v in sorted_records)
+    self.touch(output_filename)
+    meta_check = self.get_metadata("output.csv")
+    print(f"metadata immediately after touch: {meta_check._export() if meta_check else None}")
+    self.append_contents(output_filename, contents)
+    meta_check2 = self.get_metadata("output.csv")
+    print(f"metadata after append_contents: {meta_check2._export() if meta_check2 else None}")
+
+    for node in self.chord.nodes.values():
+      if job_id in node.sort_buffer:
+        del node.sort_buffer[job_id]
+
+    return f"SUCCSS: Sorted {len(sorted_records)} records from '{file_name}' into '{output_filename}'"
+        
---
+++ main.py
@@ -0,0 +1,119 @@
+# test_chord_dfs.py
+from Structures.chord import Chord, Node
+from api import DFS
+
+def test_sort(dfs):
+    print("\n=== Sort Test ===")
+
+    # create a DFS file with 10 key,value records out of order
+    records = [
+        "0042,bob",
+        "0012,alice",
+        "0190,carol",
+        "0031,dave",
+        "0055,eve",
+        "0008,frank",
+        "0100,grace",
+        "0077,heidi",
+        "0003,ivan",
+        "0200,judy",
+    ]
+
+    # write records to a local file and append into DFS
+    with open("sort_input.txt", "w") as f:
+        f.write("\n".join(records))
+
+    dfs.touch("input.csv")
+    print(dfs.append("input.csv", "sort_input.txt"))
+
+    # sort
+    print(dfs.sort_file("input.csv", "output.csv"))
+
+    # print(dfs.sort_file("input.csv", "output.csv"))
+
+    # debug
+    meta = dfs.get_metadata("output.csv")
+    print(f"output.csv metadata: {meta._export() if meta else None}")
+
+    for pk in (meta.page_keys if meta else []):
+        page = dfs.get_page(pk)
+        print(f"  page_key={pk} -> {page}")
+
+    # verify
+    print("\nsorted output:")
+    print(dfs.read("output.csv"))
+    # print(dfs.ls())
+    # print(dfs.read("input.csv"))
+
+
+
+def make_ring():
+    chord = Chord()
+    for node_id in [4, 8, 15, 27, 44, 58]:
+        node = Node(chord, node_id)
+        chord.add_node(node)
+    return chord
+
+def test_ring(chord):
+    print("=== Ring Structure ===")
+    chord.print_ring()
+    print()
+    chord.print_finger_tables()
+
+def test_dfs(dfs):
+    print("=== DFS Operations ===")
+
+    # touch
+    print(dfs.touch("music.txt"))
+    print(dfs.touch("music.txt"))  # should say already exists
+
+    # stat on empty file
+    print("\nstat after touch:")
+    print(dfs.stat("music.txt"))
+
+    # create a small local file to append
+    with open("test_input.txt", "w") as f:
+        f.write("hello world\n" * 100)
+
+    # append
+    print("\n" + dfs.append("music.txt", "test_input.txt"))
+    print("\nstat after append:")
+    print(dfs.stat("music.txt"))
+
+    # read
+    print("\nread (first 50 chars):")
+    print(dfs.read("music.txt")[:50])
+
+    # head
+    print("\nhead 3:")
+    print(dfs.head("music.txt", 3))
+
+    # tail
+    print("\ntail 3:")
+    print(dfs.tail("music.txt", 3))
+
+    # ls
+    print("\nls:")
+    print(dfs.ls())
+
+    # touch a second file
+    dfs.touch("notes.txt")
+    print("\nls after second touch:")
+    print(dfs.ls())
+
+    # delete
+    print("\n" + dfs.delete_file("music.txt"))
+    print("\nls after delete:")
+    print(dfs.ls())
+
+    # read deleted file
+    print("\nread after delete:")
+    print(dfs.read("music.txt"))
+
+if __name__ == "__main__":
+    chord = make_ring()
+    test_ring(chord)
+    entry = chord.nodes[4]
+    dfs = DFS(chord, entry)
+    test_dfs(dfs)
+    test_sort(dfs)
